52ae4dadec45f1e64780a2199d450ba8463d03ce
ndhunju <nikesh_dhunju@hotmail.com>
2022-12-31 15:42:52 -0800

    Improve ControlsLayout.java's UI and code.

--- src/main/java/com/ndhunju/folderpicker/library/ControlsLayout.java
+++ src/main/java/com/ndhunju/folderpicker/library/ControlsLayout.java
@@ -3,12 +3,16 @@ package com.ndhunju.folderpicker;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
 import android.text.TextUtils;
 import android.view.Gravity;
-import android.view.ViewGroup;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import static android.view.ViewGroup.LayoutParams.*;
 
 /**
  * ControlsLayout consist of three parts/views
@@ -18,63 +22,80 @@ import android.widget.TextView;
  */
 public class ControlsLayout extends RelativeLayout {
 
-    //Member variables
-    private ImageButton mBackImgBtn;
-    private ImageButton mNewDirImgBtn;
-    private TextView mPathTextView;
+    // Member variables
+    private final ImageButton mBackImgBtn;
+    private final ImageButton mNewDirImgBtn;
+    private final TextView mPathTextView;
 
-    //Constructor
+    // Constructor
     public ControlsLayout(@NonNull Context context){
         super(context);
 
+        // Create and add Back button
         mBackImgBtn = new ImageButton(context);
-        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        mBackImgBtn.setBackground(null);
+        mBackImgBtn.setId(View.generateViewId());
+        LayoutParams params = new LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
         params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
         mBackImgBtn.setLayoutParams(params);
-        mBackImgBtn.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_revert));
+        mBackImgBtn.setImageDrawable(ContextCompat.getDrawable(
+                context,
+                R.drawable.ic_baseline_arrow_back_24
+        ));
+        this.addView(mBackImgBtn);
 
+        // Create and add New Folder button
+        mNewDirImgBtn = new ImageButton(context);
+        mNewDirImgBtn.setId(View.generateViewId());
+        mNewDirImgBtn.setBackground(null);
+        mNewDirImgBtn.setImageDrawable(ContextCompat.getDrawable(
+                getContext(),
+                R.drawable.ic_baseline_create_new_folder_24
+        ));
+        LayoutParams newDirBtnParams = new LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+        newDirBtnParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        newDirBtnParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        mNewDirImgBtn.setLayoutParams(newDirBtnParams);
+        this.addView(mNewDirImgBtn);
+
+        // Create and add text view for showing path
         mPathTextView = new TextView(context);
-        LayoutParams params1 = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        mPathTextView.setId(View.generateViewId());
+        LayoutParams params1 = new LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
         params1.addRule(RelativeLayout.CENTER_IN_PARENT);
-        params1.addRule(RelativeLayout.RIGHT_OF, mBackImgBtn.getId());
+        params1.addRule(RelativeLayout.END_OF, mBackImgBtn.getId());
+        params1.addRule(RelativeLayout.START_OF, mNewDirImgBtn.getId());
         mPathTextView.setLayoutParams(params1);
         mPathTextView.setTextAppearance(context, android.R.style.TextAppearance_DeviceDefault);
         mPathTextView.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
         mPathTextView.setSingleLine();
         mPathTextView.setEllipsize(TextUtils.TruncateAt.START);
-
-        mNewDirImgBtn = new ImageButton(context);
-        LayoutParams params2 = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        params2.addRule(RelativeLayout.RIGHT_OF, mPathTextView.getId());
-        mNewDirImgBtn.setLayoutParams(params2);
-        mNewDirImgBtn.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_menu_add));
-
-        this.addView(mBackImgBtn);
         this.addView(mPathTextView);
-        this.addView(mNewDirImgBtn);
 
     }
 
     /**
-     * This method sets a callback which will be invoked when {@Link mBackImgBtn} is clicked
-     * @param onBackPressedListener
+     * This method sets a callback which will be invoked when
+     * {@link com.ndhunju.folderpicker.ControlsLayout#mBackImgBtn} is clicked
      */
-    public void setOnBackPressedListener(OnClickListener onBackPressedListener){
-        if(onBackPressedListener != null) mBackImgBtn.setOnClickListener(onBackPressedListener);
+    public void setOnBackPressedListener(OnClickListener onBackPressedListener) {
+        if (onBackPressedListener != null){
+            mBackImgBtn.setOnClickListener(onBackPressedListener);
+        }
     }
 
     /**
-     * This method sets a callback which will be called when {@Link mNewDirImgBtn} is clicked
-     * @param onNewDirPressedListener
+     * This method sets a callback which will be called when
+     * {@link com.ndhunju.folderpicker.ControlsLayout#mNewDirImgBtn} is clicked
      */
-    public void setOnNewDirPressedListener(OnClickListener onNewDirPressedListener){
-        if(onNewDirPressedListener != null) mNewDirImgBtn.setOnClickListener(onNewDirPressedListener);
+    public void setOnNewDirPressedListener(OnClickListener onNewDirPressedListener) {
+        if (onNewDirPressedListener != null) {
+            mNewDirImgBtn.setOnClickListener(onNewDirPressedListener);
+        }
     }
 
     /**
      * This method can be used to set custom icon for Back Button
-     * @param icon
      */
     public void setBackBtnImg(Drawable icon){
         mBackImgBtn.setImageDrawable(icon);
@@ -82,7 +103,6 @@ public class ControlsLayout extends RelativeLayout {
 
     /**
      * * This method can be used to set custom icon for New Button
-     * @param icon
      */
     public void setNewBtnImg(Drawable icon){
         mNewDirImgBtn.setImageDrawable(icon);
@@ -91,7 +111,6 @@ public class ControlsLayout extends RelativeLayout {
     /**
      * This method returns instance of TextView used to display current path.
      * It can be used to apply custom appearance
-     * @return
      */
     public TextView getPathTextView(){
         return mPathTextView;
@@ -99,7 +118,6 @@ public class ControlsLayout extends RelativeLayout {
 
     /**
      * This method sets current path with passed path
-     * @param path
      */
     public void setCurrentPath(String path){
         mPathTextView.setText(path);
